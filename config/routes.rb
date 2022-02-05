d04ea9b0571428c2db93ce8cf2625f4f50768687
Volkan Şendağ <vsendag@gmail.com>
2022-02-05 13:32:22 +0300

    add new 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
+  resources :articles
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
 end
