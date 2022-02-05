8b6c10a7534ec122194f3220d887a06e08ae71ae
Volkan Şendağ <vsendag@gmail.com>
2022-02-05 22:49:26 +0300

    comment add

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+# app/controllers/turbo_devise_controller.rb
+
+class CommentsController < ApplicationController
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+      end
+
+      def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: 303
+      end
+    
+      private
+        def comment_params
+          params.require(:comment).permit(:commenter, :body)
+        end
+end
